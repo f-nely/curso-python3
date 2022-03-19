77cbef7f54216d2195981776020192a43d2295f5
nely de faria <nelly.nf54@gmail.com>
2022-03-19 19:43:33 -0300

    Criando arquivo comando_print.py

---
+++ curso-python3/section-02/comando_print.py
@@ -0,0 +1,15 @@
+# print(3030)
+print('Luiz', 'Otávio', sep='-', end=' ')
+print('Code', 'for', 'fun', sep='-')
+
+# def Print(v):
+#     return v
+#
+#
+# print(Print('executar'))
+
+"""
+824.176.070-18
+"""
+print('824', '176', '070', sep='.', end='-')
+print('18')
