e3b5ba6545213b0916f8e1b5fece85f63d6bf283
nely de faria <nelly.nf54@gmail.com>
2022-03-20 12:59:48 -0300

    Criando arquivo input.py

---
+++ curso-python3/section-02/input.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual o seu nome? ')).title()
+idade = int(input('Qual a sua idade? '))
+
+# print(f'O usuário digitou {nome} e o tipo da variável é {type(nome)}')
+print('-' * 35)
+print(f'{nome} tem {idade} anos.')
