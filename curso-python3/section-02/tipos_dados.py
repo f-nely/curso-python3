031297947eb7df9b779710eeefad60b0c0828ea3
nely de faria <nelly.nf54@gmail.com>
2022-03-20 12:56:42 -0300

    Criando arquivo tipos_dados.py

---
+++ curso-python3/section-02/tipos_dados.py
@@ -0,0 +1,23 @@
+"""
+Tipos de dados
+str - string: textos 'something'
+int - inteiro: 3030
+float - real/ponto flutuante: 11.30
+bool - booleano/lógico: True/False
+"""
+
+print('Luis', type('Luis'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+
+print('-' * 20)
+# exercício
+name = 'james'
+print(name, type(name))
+age = 62
+print(age, type(age))
+height = 1.90
+print(height, type(height))
+print('-' * 20)
+print(age >= 18)
