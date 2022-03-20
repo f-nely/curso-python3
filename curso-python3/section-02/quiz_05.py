5356b4d855e48e74b66f1aabb6765897e9025c18
nely de faria <nelly.nf54@gmail.com>
2022-03-20 13:21:49 -0300

    Criando arquivo quiz_05.py

---
+++ curso-python3/section-02/quiz_05.py
@@ -0,0 +1,13 @@
+# num_1 = 2
+# num_2 = 1
+#
+# if num_1 = num_2:
+#     print('num_1 é igual a num_2')
+
+num_1 = 0
+num_2 = 0
+
+if not num_1 != num_2:
+    print('Retorno 1')
+else:
+    print('Retorno 2')
