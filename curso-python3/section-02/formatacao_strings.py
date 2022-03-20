9f6b699f0a136de1f850fd8ca0b90b104463b951
nely de faria <nelly.nf54@gmail.com>
2022-03-20 12:58:19 -0300

    Criando arquivo formatacao_strings.py

---
+++ curso-python3/section-02/formatacao_strings.py
@@ -0,0 +1,8 @@
+name = 'Luis Otávio'
+age = 32
+height = 1.80
+weight = 80
+imc = weight / height ** 2
+
+print(f'{name} tem {age} anos e seu imc é {imc:.2f}')
+print('{} tem {} anos e seu imc é {:.2f}'.format(name, age, imc))
