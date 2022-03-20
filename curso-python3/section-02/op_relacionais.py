d37f13a1e578013a26e73a172eff385a6429c42f
nely de faria <nelly.nf54@gmail.com>
2022-03-20 13:03:26 -0300

    Criando arquivo op_relacionais.py

---
+++ curso-python3/section-02/op_relacionais.py
@@ -0,0 +1,16 @@
+num1 = 2
+num2 = '2'
+num3 = 2
+var1 = 'Luiz'
+var2 = 'Otávio'
+
+print(num1 == num1)
+print(num1 == num2)
+print(num1 >= num3)
+print(var1 != var2)
+
+idade = 19
+if 20 <= idade <= 30:
+    print('pode pegar o empréstimo.')
+else:
+    print('não pode pegar o empréstimo.')
