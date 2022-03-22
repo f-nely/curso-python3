56f97f631f368a196d2193601fe2504b1417a430
nely de faria <nelly.nf54@gmail.com>
2022-03-22 16:45:42 -0300

    Criando arquivo for_else.py

---
+++ curso-python3/section-02/for_else.py
@@ -0,0 +1,7 @@
+names = ['Grace Hopper', 'James Gosling', 'Alan Turing']
+
+for name in names:
+    if name.startswith('J'):
+        continue
+else:
+    print('Não existe uma palavra que começa com J ')
