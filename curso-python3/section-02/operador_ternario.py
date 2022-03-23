b922470322e62781ac7440d5954c3126c6b57ccd
nely de faria <nelly.nf54@gmail.com>
2022-03-23 19:43:42 -0300

    Criando arquivo operador_ternario.py

---
+++ curso-python3/section-02/operador_ternario.py
@@ -0,0 +1,7 @@
+logged_user = False
+msg = 'Usuário logado.' if logged_user else 'Usuário precisa logar.'
+print(msg)
+
+age = 14
+result = 'Pode acessar.' if age >= 18 else 'Não pode acessar.'
+print(result)
