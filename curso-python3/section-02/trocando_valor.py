7c6c7cf15b242a560c99013345f17206868be61d
nely de faria <nelly.nf54@gmail.com>
2022-03-23 18:05:34 -0300

    Criando arquivo trocando_valor.py

---
+++ curso-python3/section-02/trocando_valor.py
@@ -0,0 +1,15 @@
+x = 10
+y = 'Luiz'
+z = 'Otávio'
+
+# aux = x
+# x = y
+# print(x)
+# y = aux
+# print(y)
+
+# x, y = y, x
+# print(f'x={x} e y={y}')
+
+x, y, z = z, x, y
+print(f'x={x} e y={y} e z={z}')
